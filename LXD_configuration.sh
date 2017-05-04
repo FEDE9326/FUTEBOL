938ec25b0d145b558be698e9ccce78d10fca1cbe
FEDE9326 <fede9326@gmail.com>
2017-05-04 15:05:31 +0100

    hdhs

--- LXD_configuration.sh
+++ LXD_configuration.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable;
-sudo apt-get update;
-sudo apt-get install lxd;
-sudo apt-get install cgroup-bin cgroup-lite cgroup-tools cgroupfs-mount libcgroup1;
-newgrp lxd;
-sudo usermod -a -G lxd federicobarusso;
-sudo apt-get install bridge-utils;
-sudo apt install criu;
+
+sudo apt-get update && sudo apt-get install -y protobuf-c-compiler libprotobuf-c0-dev protobuf-compiler libprotobuf-dev:amd64 gcc build-essential bsdmainutils python git-core asciidoc make htop git curl supervisor cgroup-lite libapparmor-dev libseccomp-dev libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev libaio-dev apparmor libnet1-dev bridge-utils cgroup-bin cgroup-lite cgroup-tools cgroupfs-mount libcgroup1 lxd;
+git clone  https://github.com/xemul/criu.git criu;
+cd criu;
+make clean;
+make;
+sudo make install;
+sudo nano /etc/network/interfaces;
+
