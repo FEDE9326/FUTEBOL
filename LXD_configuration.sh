a67dd77e3665eace588ad8b2abf41b0d6bc9da82
FEDE9326 <fede9326@gmail.com>
2017-06-08 12:51:07 +0100

    Added criu version2.12

--- LXD_configuration.sh
+++ LXD_configuration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sudo apt-get update && sudo apt-get install -y protobuf-c-compiler libprotobuf-c0-dev protobuf-compiler libprotobuf-dev:amd64 gcc build-essential bsdmainutils python git-core asciidoc make htop git curl supervisor cgroup-lite libapparmor-dev libseccomp-dev libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev libaio-dev apparmor libnet1-dev bridge-utils cgroup-bin cgroup-lite cgroup-tools cgroupfs-mount libcgroup1 lxd;
+sudo apt-get update && sudo apt-get install -y protobuf-c-compiler libprotobuf-c0-dev protobuf-compiler libprotobuf-dev:amd64 gcc build-essential bsdmainutils python git-core asciidoc make htop git curl supervisor cgroup-lite libapparmor-dev libseccomp-dev libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev libaio-dev apparmor libnet1-dev bridge-utils cgroup-bin cgroup-lite cgroup-tools cgroupfs-mount libcgroup1 lxd zfsutils-linux;
 git clone  https://github.com/xemul/criu.git criu;
 cd criu;
 make clean;
