ef0ba35e226ecbabddcaefa634a6fc1de1494a4a
FEDE9326 <fede9326@gmail.com>
2017-08-01 14:22:46 +0100

    Update receiver_script.py

--- receiver_script.py
+++ receiver_script.py
@@ -8,10 +8,12 @@ UDP_PORT = 7124
 sock = socket.socket(socket.AF_INET, # Internet
 socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
+myfile = open("data_received", "a")
 while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 by
+        myfile.write(data)
         print "received message:", data
-
+myfile.close()
 #Sender
 #cd /usr/local/lib/uhd/examples
 #./rx_samples_to_udp --freq 915e6 --rate 5e6 --gain 10 --addr 192.168.5.207 --nsamps 100000000
