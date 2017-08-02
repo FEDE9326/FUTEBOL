0e074ae823728ac2f990309d2eff85ab112bf11d
FEDE9326 <fede9326@gmail.com>
2017-08-02 18:24:41 +0100

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -59,10 +59,23 @@ LXCPATH=$(lxc-config lxc.lxcpath)
 
 lxc-checkpoint -n $name -D $checkpoint_dir -s -v
 
+network_name="virtual1";
+ip_address="192.168.5.49";
+bridge_name="BRIDGE-"$(echo $network_name | tr [a-z] [A-Z]);
+internal_ip=$(sudo lxc-info -n receiver | grep "IP:" | head -1 | sed "s/[IP: ]//g");
+
+sudo ifconfig $network_name down;
+sudo ip link delete $network_name;
+sudo iptables -t nat -D PREROUTING -p all -d $ip_address -j $bridge_name;
+sudo iptables -t nat -D OUTPUT -p all -d $ip_address -j $bridge_name;
+sudo iptables -t nat --flush $bridge_name;
+sudo iptables -t nat --delete-chain $bridge_name;
+
 do_rsync $LXCPATH/$name/
 do_rsync $checkpoint_dir/
 
 ssh -t $host "sudo lxc-checkpoint -r -n $name -D $checkpoint_dir -v"
 ssh -t $host "sudo lxc-wait -n u1 -s RUNNING"
+ssh -t $host "sudo ./up_interface.sh"
 
 fi
