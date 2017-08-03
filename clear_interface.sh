ca641935f7c0e1ed051a4107b0fd7aa1b05cef4f
FEDE9326 <fede9326@gmail.com>
2017-08-03 09:48:07 +0100

    Update clear_interface.sh

--- clear_interface.sh
+++ clear_interface.sh
@@ -9,6 +9,7 @@ sudo ifconfig $network_name down;
 sudo ip link delete $network_name;
 sudo iptables -t nat -D PREROUTING -p all -d $ip_address -j $bridge_name;
 sudo iptables -t nat -D OUTPUT -p all -d $ip_address -j $bridge_name;
+sudo iptables -t nat -D POSTROUTING -p all -s  $internal_ip -j SNAT --to-source $ip_address;
 sudo iptables -t nat --flush $bridge_name;
 sudo iptables -t nat --delete-chain $bridge_name;
 
