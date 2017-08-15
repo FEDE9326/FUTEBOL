159a9cd58752c41d20fcef3bfa6ebf2a50a4cefa
FEDE9326 <fede9326@gmail.com>
2017-08-15 17:57:54 +0100

    Update receiver_script.py

--- receiver_script.py
+++ receiver_script.py
@@ -1,61 +1,78 @@
-# Python receiver 
-
 #!/usr/bin/python
+
+# Python receiver
 
 import socket
 import os
 import sys
-import socket.timeout as TimeoutException
+import threading
+import time
 
+stop = 0
 rate = sys.argv[1]
 nsamps = sys.argv[2]
+sleep_time = 4
+
+def receive_stop():
+        print "running the thread..."
+        sock_tcp = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock_tcp.bind((IP_REC,TCP_PORT))
+        sock_tcp.listen(1)
+        connection, client_address = sock_tcp.accept()
+
+        global stop
+
+        while True:
+                data=connection.recv(16)
+                if data == "stop":
+                        print "collecting the last packets..."
+                        time.sleep(sleep_time)
+                        stop = 1
+                        break
+        return
+
 
 f = os.popen('ifconfig eth0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
-UDP_IP_REC = f.read()
+IP_REC = f.read()
 #UDP_IP_SEND = "192.168.5.86"
 UDP_PORT_rec = 7124
 #UDP_PORT_send = 4567
 TCP_IP = "192.168.5.48"
 TCP_PORT = 7890
-
-wait_time=10
+wait_time = 2
 
 sock_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-sock_rec.bind((UDP_IP, UDP_PORT))
+sock_rec.bind((IP_REC, UDP_PORT_rec))
 sock_rec.settimeout(wait_time)
 
 #sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock_tcp = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock_tcp.bind(REC_ADDRESS,TCP_PORT)
-
-sock_tcp.listen()
-connection, client_adress = sock_tcp.accept()
+sock_tcp.connect((TCP_IP,TCP_PORT))
 
 try:
-	sock_tcp.sendall("start")
+        sock_tcp.sendall("start")
 except socket.error:
-	print "Network errors"
+        print "Network errors"
+sock_tcp.close()
 
-before = os.popen("netstat --udp -i | grep eth0 | awk '{print $4}'")
-UDP_before= before.read()
+t = threading.Thread(target=receive_stop)
+t.start()
 
-while True:
-	try:
-       		data = sock_rec.recvfrom(1024) # buffer size is 1024 by
-       	except TimeoutException:
-       		print "Timeout exiting"
-       		break
-        	#sock_send.sendto(data, (UDP_IP_SEND,UDP_PORT_send))
-        # print "received message:", data
+before = os.popen("netstat -suno | grep 'packets received' | awk '{print $1}'")
+UDP_before = int(before.read())
 
-after = os.popen("netstat --udp -i | grep eth0 | awk '{print $4}'")
-UDP_after = after.read()
+while stop == 0:
+        try:
+                data = sock_rec.recvfrom(1024) # buffer size is 1024 by
+        except socket.error:
+                print "I'm not receiving data..."
 
-f=open("results_"+rate+"_"+nsamps+".dat","w")
-f.write(rate+" "+nsamps+" "+UDP_after-UDP_before)
+#sock_send.sendto(data, (UDP_IP_SEND,UDP_PORT_send))
+        #print "received message:", stop
+
+after = os.popen("netstat -suno | grep 'packets received' | awk '{print $1}'")
+UDP_after = int(after.read())
+
+f = open("results_"+rate+"_"+nsamps+".dat","a")
+f.write(rate + " " + nsamps + " " + str(UDP_after-UDP_before) + "\n")
 f.close()
-
-
-
-
-
