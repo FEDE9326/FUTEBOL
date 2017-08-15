a6ab1d559188397630cef29b2d5e4328540c7d95
FEDE9326 <fede9326@gmail.com>
2017-08-15 13:13:28 +0100

    Update receiver_script.py

--- receiver_script.py
+++ receiver_script.py
@@ -1,42 +1,52 @@
-#!/usr/bin/python
+# Python receiver 
 
+#!/usr/bin/python
 
 import socket
 import os
 import sys
-import time
-import subprocess
+import socket.timeout as TimeoutException
 
 rate = sys.argv[1]
 nsamps = sys.argv[2]
 
-REC_ADDRESS = "192.168.5.49"
-TCP_PORT = "7890"
-cmd = "/usr/local/lib/uhd/examples/rx_samples_to_udp --freq 915e6 --rate " + rate + "--gain 10 --addr " +  REC_ADDRESS + "--nsamps " + nsamps
+f = os.popen('ifconfig eth0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
+UDP_IP_REC = f.read()
+#UDP_IP_SEND = "192.168.5.86"
+UDP_PORT_rec = 7124
+#UDP_PORT_send = 4567
+TCP_IP = "192.168.5.48"
+TCP_PORT = 7890
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(REC_ADDRESS,TCP_PORT)
+wait_time=10
 
-s.listen()
-connection, client_adress = s.accept()
+sock_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock_rec.bind((UDP_IP, UDP_PORT))
+sock_rec.settimeout(wait_time)
 
+#sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_tcp = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock_tcp.bind(REC_ADDRESS,TCP_PORT)
 
-try: 
-	while True:
-		data=connection.recv(16)
-		if data=="start":
-			break
-		else:
-			print "waiting for starting..."
-			time.sleep(1)
-	
+sock_tcp.listen()
+connection, client_adress = sock_tcp.accept()
+
+try:
+	sock_tcp.sendall("start")
+except socket.error:
+	print "Network errors"
+
 before = os.popen("netstat --udp -i | grep eth0 | awk '{print $8}'")
 UDP_before= before.read()
 
-# RUNNING the command
-p = subprocess.Popen(cmd)
-print "running the script..."
-p.wait()
+while True:
+		try:
+       		data = sock_rec.recvfrom(1024) # buffer size is 1024 by
+       	except TimeoutException:
+       		print "Timeout exiting"
+       		break
+        	#sock_send.sendto(data, (UDP_IP_SEND,UDP_PORT_send))
+        # print "received message:", data
 
 after = os.popen("netstat --udp -i | grep eth0 | awk '{print $8}'")
 UDP_after = after.read()
@@ -45,4 +55,7 @@ f=open("results_"+rate+"_"+nsamps+".dat","w")
 f.write(rate+" "+nsamps+" "+UDP_after-UDP_before)
 f.close()
 
-s.close()
+
+
+
+
