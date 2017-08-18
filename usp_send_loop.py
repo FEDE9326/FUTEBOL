6e94959a388e30270f6f644feca22aaac08ae6b5
FEDE9326 <fede9326@gmail.com>
2017-08-18 16:43:36 +0100

    Update usp_send_loop.py

--- usp_send_loop.py
+++ usp_send_loop.py
@@ -15,10 +15,10 @@ f = os.popen('ifconfig eth0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
 MY_ADDRESS = f.read()
 REC_ADDRESS = "192.168.5.49"
 TCP_PORT = 7890
-ERROR_MESSAGE="Error: send: Connection refused"
-cmd = '/usr/local/lib/uhd/examples/rx_samples_to_udp --freq 915e6 --rate ' + rate + ' --gain 10 --addr ' 
-+  REC_ADDRESS + ' --nsamps ' + nsamps + ' | grep "Error: send: Connection refused"' 
-lista=cmd.split(" ")
+ERROR_MESSAGE = "Connection refused"
+cmd = '/usr/local/lib/uhd/examples/rx_samples_to_udp --freq 915e6 --rate ' + rate + ' --gain 10 --addr ' +  REC_ADDRESS + ' --nsamps ' + nsamps + ' | grep "Error: send: Connection refused"' 
+lista = cmd.split(" ")
+iteration = 0
 
 while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,19 +40,21 @@ while True:
         before = os.popen("netstat --udp -i | grep eth0 | awk '{print $8}'")
         UDP_before = int(before.read())
         # RUNNING the command
-	
+
 	start = time.time()
         p = subprocess.Popen(lista,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print "running the script..."
         p.wait()
-	
-	if stdout.read() == ERROR_MESSAGE:
+
+	out,err=p.communicate()
+
+	if ERROR_MESSAGE in err:
 		new_time = time.time() - start
-		lista[9] = int(lista[9]-(new_time*nsamps/sim_time))
+		lista[10] = str(int(int(lista[10])-(float(new_time)*int(nsamps)/int(sim_time))))
 		p = subprocess.Popen(lista)
 		print "running the script...2 time for a network error"
 		p.wait()
-	
+
         print "sending the stop command"
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((REC_ADDRESS,TCP_PORT))
@@ -61,11 +63,11 @@ while True:
 
         after = os.popen("netstat --udp -i | grep eth0 | awk '{print $8}'")
         UDP_after = int(after.read())
-
+	iteration=iteration+1
         f=open("results_"+rate+"_"+nsamps+".dat","a")
-        f.write(rate+" " + nsamps + " " + str(UDP_after-UDP_before) + "\n")
+        f.write(str(iteration) + " " + rate+" " + nsamps + " " + str(UDP_after-UDP_before) + "\n")
         f.close()
 	
 	s.close()
-	
+	time.sleep(3)
 
