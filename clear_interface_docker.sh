77df4c079eec5d6549b7595d77cd7ebfc26a181e
FEDE9326 <fede9326@gmail.com>
2017-08-26 18:41:36 +0100

    Update clear_interface_docker.sh

--- clear_interface_docker.sh
+++ clear_interface_docker.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+network_name="virtual1";
+ip_address="192.168.5.49";
+bridge_name="BRIDGE-"$(echo $network_name | tr [a-z] [A-Z]);
+internal_ip="172.17.0.2";
+
+sudo ifconfig $network_name down;
+sudo ip link delete $network_name;
+sudo iptables -t nat -D PREROUTING -p all -d $ip_address -j $bridge_name;
+sudo iptables -t nat -D OUTPUT -p all -d $ip_address -j $bridge_name;
+sudo iptables -t nat -D POSTROUTING -p all -s  $internal_ip -j SNAT --to-source $ip_address;
+sudo iptables -t nat --flush $bridge_name;
+sudo iptables -t nat --delete-chain $bridge_name;
+
+echo "DONE";
