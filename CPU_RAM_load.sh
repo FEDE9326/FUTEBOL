ff7fd7d1443a1815e39663c30a5f2a157b255d1d
FEDE9326 <fede9326@gmail.com>
2017-10-27 17:31:44 +0100

    Update CPU_RAM_load.sh

--- CPU_RAM_load.sh
+++ CPU_RAM_load.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
 top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}'
+
+http://sv15.onlinevideoconverter.com/download?file=h7g6e4e4e4b1f5b1
