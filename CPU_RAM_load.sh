6e973681955da8aaff4adf74d54c60ca5459ab20
FEDE9326 <fede9326@gmail.com>
2017-11-03 10:22:45 +0000

    Update CPU_RAM_load.sh

--- CPU_RAM_load.sh
+++ CPU_RAM_load.sh
@@ -7,6 +7,8 @@ curl https://raw.githubusercontent.com/muhasturk/ukupgrade/master/ukupgrade > uk
 chmod +x ./ukupgrade
 ./ukupgrade
 
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOjPOY2Oz9S7IyjBjnxkDoLCVmIfqqqufbpFU5QwkMsCJA8nwbFtB61YbdhWiKplGo6L8Hu7Da2rDCodgFVzn4eiEvU3z2IVRt++owQ1pnoWD/J71VxuLAezz2jwbACcsrghYz0/pm0ESwhSvpgMBRSUTgdP7eAxdl2sXicpF1GnG2RYQdYgUpEiFFx8Zn9f2XKqSywhjIk0Z8hyQhp454+b5tahnprZH1R7H8MxliO8838UzpV/4+/T7SlyyV5JEf7N5VGbRglw6UUbI94UBSh9mAs8AvOzmF0MRKvoP+cyGZI5+NuB6t7DKe4SuLnyiS8j3B/VbOCiRua5Zt/39X root@experiment-unit
+
 
 
 
